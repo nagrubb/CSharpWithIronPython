7317d3c31157d8d0a75f41cc444ca1e8314bd79e
Nathan Grubb <mrnathangrubb@gmail.com>
2017-11-05 07:08:36 -0800

    Initial Commit

---
+++ CSharpConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using IronPython.Hosting;
+
+namespace CSharpConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World From C#");
+            var py = Python.CreateEngine();
+            try
+            {
+                py.Execute("print('Hello World From Python')");
+            }
+            catch
+            {
+                Console.WriteLine("Wups!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
